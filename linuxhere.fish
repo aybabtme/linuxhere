2b83f8430499ddb61a837afae8d42ef558fab3fb
Antoine Grondin <antoinegrondin@gmail.com>
2017-07-20 11:58:08 +0200

    pull image if missing

--- linuxhere.fish
+++ linuxhere.fish
@@ -3,6 +3,11 @@ function linuxhere --description 'Runs linux in the current directory'
 	if test $argv[1]
 		set distro $argv[1]
 	end
+
+	if test (docker images --filter "reference=$distro" --format "{{.Repository}}:{{.Tag}}") == ""
+		docker pull $distro
+	end
+
 	echo -e "\x1b[1;94mRunning \x1b[0;1;3;4;96m$distro\x1b[0;1;94m here!\x1b[0m"
-	docker run --hostname (hostname)"-on-linux" -v /:/there/ --workdir /there/(pwd) -i -t $distro  /bin/bash
+	docker run --rm --hostname (hostname)"-on-linux" -v /:/there/ --workdir /there/(pwd) -i -t $distro  /bin/bash
 end
